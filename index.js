5d8b48203cb8c3e1e2b0d5354dbb2313d623a2d3
rayhomie <1572801584@qq.com>
2021-07-23 10:25:48 +0800

    打印jsx

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello");
+class React {
+  static createElement(tag, attrs, ...children) {
+    return {
+      tag,
+      attrs,
+      children,
+    };
+  }
+}
+
+const ele = (
+  <div className="active" title="123">
+    hello,<span>react</span>
+  </div>
+);
+console.log(ele);
